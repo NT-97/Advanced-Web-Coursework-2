7340d66d2febe7c059cb55a08c71723d69c6dcd2
Nicholas Taylor <40203073@live.napier.ac.uk>
2018-11-16 17:56:44 +0000

    database connnection created

--- sourcecode/main.py
+++ sourcecode/main.py
@@ -1,30 +1,83 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, g
 from datetime import date, timedelta
 from calendar import monthrange
+import sqlite3
 
 app = Flask(__name__)
+# Setting the path to the database
+db_location = 'var/db.db'
 
+# Function to get the current database connection
+def get_db():
+  db = getattr(g, 'db', None)
+  if db is None:
+    db = sqlite3.connect(db_location)
+    g.db = db
+    # Return database results as Row objects for easier operation
+    db.row_factory = sqlite3.Row
+  return db
+
+# Function to query the database
+def query_db(query, args=(), one=False):
+  cur = get_db().execute(query, args)
+  rv = cur.fetchall()
+  cur.close()
+  return (rv[0] if rf else None) if one else rv
+
+# Function to close the database connection when the context is destroyed
+@app.teardown_appcontext
+def close_db_connection(exception):
+  db = getattr(g, 'db', None)
+  if db is not None:
+    db.close()
+
+# Function to initialise the database
+def init_db():
+  with app.app_context():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
+
+# Main route of the application that is used to display current and other
+# month's views
 @app.route('/')
-@app.route('/<int:year>/<int:month>')
+@app.route('/<int:year>/<int:month>', methods=['GET', 'POST'])
 def root(year=None, month=None):
 
-  
+  sql = "SELECT * FROM reminders"
+  for row in query_db(sql):
+    print(row[0])
+
+  # Setting today's date as the current date
   general = date.today()
-  
+  # If year and month variables have been specified, 
+  # set the passed year and month as the current ones
   if year and month:
     general = general.replace(year=year, month=month)
 
- 
+  # Calculating the last day in the current month
   endDay = monthrange(general.year, general.month)[1]
-  
+  # Finding the next month by adding a day to the last day of the month
   nextMonth = general.replace(day=endDay) + timedelta(days=1)
-
+  # Calculating the weekday of the first day
   firstDayofWeek = monthrange(general.year, general.month)[0]
-  
+  # Calculating the previous month by substracting 1 day from the first day of
+  # the current month
   previousMonth = general.replace(day=1) - timedelta(days=1)
-  
+
+  # Updating the calendar if the user POST'ed a request
+  if request.method == 'POST':
+    print ("posted")
+
+  # Passing the arguments into the template
   return render_template('main.html', general=general, last=endDay,
   following=nextMonth, first=firstDayofWeek, previous=previousMonth)
+
+# Route for displaying the details of a day
+@app.route('/<int:year>/<int:month>/<int:day>')
+def day(year, month, day):
+  return render_template('day.html', year=year, month=month, day=day)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
